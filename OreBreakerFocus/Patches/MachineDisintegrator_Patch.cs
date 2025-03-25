f3f4647119093049bd27be2a8d9fac715d7fed93
Desperationfighter <patrick_as_desp@yahoo.de>
2025-03-25 16:01:25 +0100

    Add project files.

---
+++ OreBreakerFocus/Patches/MachineDisintegrator_Patch.cs
@@ -0,0 +1,210 @@
+﻿using HarmonyLib;
+using RecycletoInventory;
+using SpaceCraft;
+
+/*
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Instance Name> OreBreaker1(Clone)
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Intervall Spawn set to - 130 - before
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Set Tier 1 Intervall - 80
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Intervall Spawn set to - 130 - after
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Instance Name> OreBreaker1(Clone)
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Break into Pieces set to - 3 - before
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Set Tier 1 Breaker how many Ores - 3
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Breaker how many Ores set to - 3 - after
+
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Instance Name> OreBreaker2(Clone)
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Intervall Spawn set to - 90 - before
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Set Tier 2 Intervall - 75
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Intervall Spawn set to - 90 - after
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Instance Name> OreBreaker2(Clone)
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Break into Pieces set to - 4 - before
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Set Tier 2 Breaker how many Ores - 4
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Breaker how many Ores set to - 4 - after
+
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Instance Name> OreBreaker3(Clone)
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Intervall Spawn set to - 70 - before
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Set Tier 3 Intervall - 75
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Intervall Spawn set to - 70 - after
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Instance Name> OreBreaker3(Clone)
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Break into Pieces set to - 5 - before
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Set Tier 3 Breaker how many Ores - 5
+[Debug  :Ore Breaker Focus] [Ore Breaker Focus]:(MachineDisintegrator) : Breaker how many Ores set to - 5 - after
+ */
+
+namespace OreBreakerFocus.Patches
+{
+    [HarmonyPatch(typeof(MachineDisintegrator))]
+    static public class MachineDisintegrator_Patch
+    {
+        static public bool loadedonce;
+
+        static public void SettoOriginal_tick(MachineDisintegrator __instance)
+        {
+            if (!__instance.name.Contains("OreBreaker1"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 1 Intervall back to Orignal");
+
+                __instance.breakEveryXSec = Plugin.OriginalBreakIntervaltier1;
+            }
+            else if (!__instance.name.Contains("OreBreaker2"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 2 Intervall back to Orignal");
+                __instance.breakEveryXSec = Plugin.OriginalBreakIntervaltier2;
+            }
+            else if (!__instance.name.Contains("OreBreaker3"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 3 Intervall back to Orignal");
+                __instance.breakEveryXSec = Plugin.OriginalBreakIntervaltier3;
+            }
+        }
+
+        static public void SettoOriginal_XIngredientsBack(MachineDisintegrator __instance)
+        {
+            if (__instance.name.Contains("OreBreaker1"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 1 Breaker how many Ores - back to Orignal");
+                __instance.giveXIngredientsBack = Plugin.OriginalBreakintoPartstier1;
+            }
+            else if (__instance.name.Contains("OreBreaker2"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 2 Breaker how many Ores - back to Orignal");
+                __instance.giveXIngredientsBack = Plugin.OriginalBreakintoPartstier2;
+            }
+            else if (__instance.name.Contains("OreBreaker3"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 3 Breaker how many Ores - back to Orignal");
+                __instance.giveXIngredientsBack = Plugin.OriginalBreakintoPartstier3;
+            }
+        }
+
+        static public void SettoModded_tick(MachineDisintegrator __instance)
+        {
+            Plugin.MyDebugLogger($"(MachineDisintegrator) : Instance Name> {__instance.name}");
+            Plugin.MyDebugLogger($"(MachineDisintegrator) : Intervall Spawn set to - {__instance.breakEveryXSec} - before");
+
+            if (__instance.name.Contains("OreBreaker1"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 1 Intervall - {Plugin.BreakIntervaltier1.Value}");
+                __instance.breakEveryXSec = Plugin.BreakIntervaltier1.Value;
+            }
+            else if (__instance.name.Contains("OreBreaker2"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 2 Intervall - {Plugin.BreakIntervaltier2.Value}");
+                __instance.breakEveryXSec = Plugin.BreakIntervaltier2.Value;
+            }
+            else if (__instance.name.Contains("OreBreaker3"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 3 Intervall - {Plugin.BreakIntervaltier3.Value}");
+                __instance.breakEveryXSec = Plugin.BreakIntervaltier3.Value;
+            }
+
+            Plugin.MyDebugLogger($"(MachineDisintegrator) : Intervall Spawn set to - {__instance.breakEveryXSec} - after");
+        }
+
+        static public void SettoModded_XIngredientsBack(MachineDisintegrator __instance)
+        {
+            Plugin.MyDebugLogger($"(MachineDisintegrator) : Instance Name> {__instance.name}");
+            Plugin.MyDebugLogger($"(MachineDisintegrator) : Break into Pieces set to - {__instance.giveXIngredientsBack} - before");
+
+            if (__instance.name.Contains("OreBreaker1"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 1 Breaker how many Ores - {Plugin.BreakintoPartstier1.Value}");
+                __instance.giveXIngredientsBack = Plugin.BreakintoPartstier1.Value;
+            }
+            else if (__instance.name.Contains("OreBreaker2"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 2 Breaker how many Ores - {Plugin.BreakintoPartstier2.Value}");
+                __instance.giveXIngredientsBack = Plugin.BreakintoPartstier2.Value;
+            }
+            else if (__instance.name.Contains("OreBreaker3"))
+            {
+                Plugin.MyDebugLogger($"(MachineDisintegrator) : Set Tier 3 Breaker how many Ores - {Plugin.BreakintoPartstier3.Value}");
+                __instance.giveXIngredientsBack = Plugin.BreakintoPartstier3.Value;
+            }
+
+            Plugin.MyDebugLogger($"(MachineDisintegrator) : Breaker how many Ores set to - {__instance.giveXIngredientsBack} - after");
+        }
+}
+
+    //Hook in Initial where the Developer Manipulate the Tick, so that should be safe.
+    [HarmonyPatch(typeof(MachineDisintegrator))]
+    [HarmonyPatch(nameof(MachineDisintegrator.SetDisintegratorInventory))]
+    static class MachineDisintegrator_SetDisintegratorInventory_Patch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(MachineDisintegrator __instance)
+        {
+            if (!Plugin.ModisActive.Value)
+            {
+                return true;
+            }
+            //Make sure to only Patch the Extractors
+            if (!__instance.name.Contains("OreBreaker")) return true;
+            if (!MachineDisintegrator_Patch.loadedonce)
+            {
+                MachineDisintegrator_Patch.loadedonce = true;
+            }
+            //Set Tick to Modded one
+            if (Plugin.ModifyTick.Value)
+            {
+                MachineDisintegrator_Patch.SettoModded_tick(__instance);
+            }
+            else
+            {
+                MachineDisintegrator_Patch.SettoOriginal_tick(__instance);
+            }
+            //Set Break to Modded one
+            if(Plugin.ModifyBreakintoXParts.Value) 
+            {
+                MachineDisintegrator_Patch.SettoModded_XIngredientsBack(__instance);
+            }
+            else
+            {
+                MachineDisintegrator_Patch.SettoOriginal_XIngredientsBack(__instance);
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(MachineDisintegrator))]
+    [HarmonyPatch(nameof(MachineDisintegrator.TryToDesintegrateAnObjectInInventory))]
+    static class MachineDisintegrator_TryToDesintegrateAnObjectInInventory_Patch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(MachineDisintegrator __instance)
+        {
+            if (!__instance.name.Contains("OreBreaker")) return true;
+
+            if (!Plugin.ModisActive.Value)
+            {
+                if (MachineDisintegrator_Patch.loadedonce)
+                {
+                    MachineDisintegrator_Patch.SettoOriginal_tick(__instance);
+                    MachineDisintegrator_Patch.SettoOriginal_XIngredientsBack(__instance);
+                }
+                return true;
+            }
+            
+            MachineDisintegrator_Patch.loadedonce = true;
+            //Set Tick to Modded one
+            if (Plugin.ModifyTick.Value)
+            {
+                MachineDisintegrator_Patch.SettoModded_tick(__instance);
+            }
+            else
+            {
+                MachineDisintegrator_Patch.SettoOriginal_tick(__instance);
+            }
+            //Set Break to Modded one
+            if (Plugin.ModifyBreakintoXParts.Value)
+            {
+                MachineDisintegrator_Patch.SettoModded_XIngredientsBack(__instance);
+            }
+            else
+            {
+                MachineDisintegrator_Patch.SettoOriginal_XIngredientsBack(__instance);
+            }
+            return true;
+        }
+    }
+}
---
+++ OreBreakerFocus/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RecycletoInventory
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+        public const string GUID = "Desperationfighter.TPC.OreBreakerFocus";
+        public const string Name = "Ore Breaker Focus";
+        public const string Version = "1.0.0.0"; //Remmber to Update Assembly Version too !
+
+        public static ConfigEntry<bool> ModisActive;
+        public static ConfigEntry<bool> ModifyTick;
+        public static ConfigEntry<bool> ModifyBreakintoXParts;
+        //public static ConfigEntry<int> UseStage;
+        //public static ConfigEntry<int> Breakluck;
+        public static ConfigEntry<int> BreakIntervaltier1;
+        public static ConfigEntry<int> BreakIntervaltier2;
+        public static ConfigEntry<int> BreakIntervaltier3;
+        public static ConfigEntry<int> BreakintoPartstier1;
+        public static ConfigEntry<int> BreakintoPartstier2;
+        public static ConfigEntry<int> BreakintoPartstier3;
+        public static ConfigEntry<bool> Debuglogging;
+
+        public const int OriginalBreakIntervaltier1 = 130;
+        public const int OriginalBreakIntervaltier2 = 90;
+        public const int OriginalBreakIntervaltier3 = 70;
+        public const int OriginalBreakintoPartstier1 = 3;
+        public const int OriginalBreakintoPartstier2 = 4;
+        public const int OriginalBreakintoPartstier3 = 5;
+
+        private void Awake()
+        {
+            ModisActive = Config.Bind("1_General", "ModisActive", true, "Set if the Mod should running or not. Should work while Game is running. Not gurantee");
+            ModifyTick = Config.Bind("2_Config", "ModifyTick", true, "Set if you want to Manipulate this speicifc Mod Feature");
+            ModifyBreakintoXParts = Config.Bind("2_Config", "ModifyBreakintoXParts", true, "Set if you want to Manipulate this speicifc Mod Feature");
+            
+            //          UseStage = Config.Bind("2_Usage", "UseStage", 2, "Set wanted usage Methode. " +
+            //   "0 = Just adjust Interval and let everything else Vanilla; " +
+            //   "1 = Extract only Primary Ore and NEVER get any by Product; " +
+            //   "2 = Focus on Primary ore. You still get a small amount of by product Ores");
+            //          Breakluck = Config.Bind("4_Stage2-Subconfig", "Breakluck", 2, "This config only applies when UseStage 2 is used. Its a 1 of X mechanic when you get eventually byproducts. For example Setting this to 1 mean 1 of 1 time you get eventually byproduct. Setting it on 2 means 1 of 2 Times you get eventually byproduct. 3 => 1 of 3 times eventually byproduct. In Percentage that means 33% of times you get a eventually random byproduct. But 2 Times gurantee the Primary Ore.");
+
+            BreakIntervaltier1 = Config.Bind("3_Tick", "BreakIntervaltier1", 130, "Affect Tier 1 Ore Breaker -> Default value in Game = 130; Time span between two Ore break tries.");
+            BreakIntervaltier2 = Config.Bind("3_Tick", "BreakIntervaltier2", 90, "Affect Tier 2 Ore Breaker -> Default value in Game = 90; Time span between two break tries.");
+            BreakIntervaltier3 = Config.Bind("3_Tick", "BreakIntervaltier3", 70, "Affect Tier 3 Ore Breaker -> Default value in Game = 70; Time span between two break tries.");
+            BreakintoPartstier1 = Config.Bind("5_BreakintoParts", "BreakintoParts1", 3, "Affect Tier 1 Ore Breaker -> Default value in Game = 3; Modify how much Ores you get back per Break.");
+            BreakintoPartstier2 = Config.Bind("5_BreakintoParts", "BreakintoParts2", 4, "Affect Tier 2 Ore Breaker -> Default value in Game = 4; Modify how much Ores you get back per Break.");
+            BreakintoPartstier3 = Config.Bind("5_BreakintoParts", "BreakintoParts3", 5, "Affect Tier 3 Ore Breaker -> Default value in Game = 3; Modify how much Ores you get back per Break.");
+            
+            Debuglogging = Config.Bind("9_Advanced", "Debuglogging", false, "Enables Debug Logging. Should be only activated when you know what you do.");
+
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+            Logger.LogInfo($"Plugin {GUID} is loaded!");
+        }
+
+        public static void MyDebugLogger(string message)
+        {
+            if (Debuglogging.Value)
+            {
+                Logger.LogDebug($"{message}");
+            }
+        }
+    }
+}
